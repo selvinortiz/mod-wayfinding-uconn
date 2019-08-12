968e7e671a583cb80ed86af7ed9c157a9741eb8d
Selvin Ortiz <selvin@selvin.co>
2019-08-12 15:43:31 -0500

    Add navbar config in store's base theme

--- src/js/store/app.js
+++ src/js/store/app.js
@@ -11,6 +11,12 @@ const defaultState = {
     main: {},
     footer: {
       nav: {
+        links: [
+          { title: 'Wayfinding', route: 'places', icon: 'places.svg' },
+          { title: 'Directory', route: 'people', icon: 'people.svg' },
+          { title: 'Bus Tracker', url: 'https://uconn-test.modolabs.net/myuconn/bus_tracker_transloc/index', icon: 'bus.svg' },
+          { title: 'Search', route: 'search', icon: 'search' }
+        ],
         item: {
           bg: '#fff',
           text: '#3daefc',
