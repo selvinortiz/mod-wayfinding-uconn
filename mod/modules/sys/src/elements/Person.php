78cbfeef687feeca78ab3a18014cd9ef92d84c01
Selvin Ortiz <selvin@selvin.co>
2019-08-22 15:34:05 -0500

    Fix issue where could not be queried due to incorrect default definition for Element type

--- mod/modules/sys/src/elements/Person.php
+++ mod/modules/sys/src/elements/Person.php
@@ -8,7 +8,7 @@ namespace modules\sys\elements;
  */
 class Person extends Element
 {
-    const TYPE_HANDLE    = 'people';
+    const TYPE_HANDLE    = 'person';
     const SECTION_HANDLE = 'people';
 
     public static function getQuery()
