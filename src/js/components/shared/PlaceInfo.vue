e80c3f2948b1287d2f73bec6e111cb9d9672ffaf
Selvin Ortiz <selvin@selvin.co>
2019-09-01 21:52:43 -0500

    Clean up PlaceInfo component and add compiled assets

--- src/js/components/shared/PlaceInfo.vue
+++ src/js/components/shared/PlaceInfo.vue
@@ -1,78 +1,70 @@
 <template>
-	<div>
+  <div :style="containerStyles">
+    <people-place-header heading="WAYFINDING" classes="hidden lg:flex"></people-place-header>
 
-		<people-place-header heading="WAYFINDING" classes="hidden lg:flex"></people-place-header>
+    <div class="w-full flex flex-wrap justify-center p-4">
+      <div class="w-full mb-4 text-2xl font-black">{{place.title}}</div>
 
-		<div class="w-full flex flex-wrap justify-center p-4">
+      <!-- Half -->
+      <div class="w-1/2">
+        <img class="w-4/5 mb-4" src="http://placehold.it/500" />
 
-			<div :class="'w-full mb-4 text-2xl font-black text-'+primaryColor">
-				{{place.title}}
-			</div>
+        <div class="font-bold mb-4">Phone Number</div>
+        <div class="font-bold">Address Line 1</div>
+        <div class="font-bold mb-4">Address Line 2</div>
+        <div>Place Description</div>
+      </div>
 
-			<!-- Half -->
-			<div class="w-1/2">
+      <!-- Half -->
+      <div class="w-1/2 text-center">
+        <select :class="'w-full flex items-center h-12 px-6 border-2 border-'+primaryColor">
+          <option class="hidden" value disabled selected>Choose Destination</option>
+        </select>
 
-				<img class="w-4/5 mb-4" src="http://placehold.it/500"/>
-
-				<div :class="'font-bold mb-4 text-'+primaryColor">Phone Number</div>
-
-				<div :class="'font-bold text-'+primaryColor">Address Line 1</div>
-				<div :class="'font-bold mb-4 text-'+primaryColor">Address Line 2</div>
-
-				<div :class="'text-'+primaryColor">Place Description</div>
-
-			</div>
-
-			<!-- Half -->
-			<div class="w-1/2 text-center">
-
-				<select :class="'w-full flex items-center h-12 px-6 border-2 border-'+primaryColor">
-				<option class="hidden" value="" disabled selected>Choose Destination</option>
-				</select>
-
-				<div :class="'text-'+primaryColor">
-				Don't see what you're looking for?
-				<u @click="() => $store.state.app.searchIsOpen = true">Switch to SEARCH</u>
-				</div>
-
-			</div>
-
-		</div>
-
-	</div>
+        <div>
+          Don't see what you're looking for? <a @click="() => $store.state.app.searchIsOpen = true">Switch to SEARCH</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import PeoplePlaceHeader from '../../components/shared/PeoplePlaceHeader.vue'
+import PeoplePlaceHeader from "../../components/shared/PeoplePlaceHeader.vue";
 
 export default {
-	components: {
-		PeoplePlaceHeader
-	},
-	props: {
-		place: {
-			type: Object,
-			default: () => ({
-				id: 0,
-				title: '',
-				maps: [
-				{
-					image: ''
-				},
-				{
-					markers: [
-					{ width: 0, height: 0, x: 0, xr: 0, y: 0,yr: 0 }
-					]
-				}
-				],
-				loaded: false
-			})
-		}
+  components: {
+    PeoplePlaceHeader
   },
-  data() {
-	return {
-		primaryColor: this.$store.state.app.theme.main.wayfinding.text
-	}
+  props: {
+    place: {
+      type: Object,
+      default: () => ({
+        id: 0,
+        title: "",
+        maps: [
+          {
+            image: ""
+          },
+          {
+            markers: [{ x: 0, y: 0 }]
+          }
+        ]
+      })
+    }
+  },
+  computed: {
+    theme() {
+      return this.$store.state.app.theme
+    }
+  },
+  methods: {
+    containerStyles() {
+      return [
+        `background-color: white`,
+        `color: black`
+      ].join(';')
+    }
   }
-}
+};
 </script>
--- web/js/person.js
+++ web/js/person.js
@@ -1,5 +1,17 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["person"],{
 
+/***/ "./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/index.js?!./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=script&lang=js&":
+/*!*******************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options!./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=script&lang=js& ***!
+  \*******************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  props: {\n    heading: {\n      type: String,\n      default: null\n    },\n    classes: {\n      type: String,\n      default: null\n    }\n  },\n\n  data() {\n    return {\n      primaryColor: this.$store.state.app.theme.main.peoplePlaceHeader.text\n    };\n  }\n\n});\n\n//# sourceURL=webpack:///./src/js/components/shared/PeoplePlaceHeader.vue?./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
 /***/ "./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/index.js?!./src/js/components/shared/PersonInfo.vue?vue&type=script&lang=js&":
 /*!************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options!./src/js/components/shared/PersonInfo.vue?vue&type=script&lang=js& ***!
@@ -24,6 +36,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _uti
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72&":
+/*!*********************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72& ***!
+  \*********************************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return staticRenderFns; });\nvar render = function() {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _c(\"div\", { class: \"w-full flex-wrap pl-4 md:mb-4 \" + _vm.classes }, [\n    _c(\n      \"div\",\n      { class: \"w-full text-3xl text-\" + _vm.primaryColor + \" font-300\" },\n      [_vm._v(_vm._s(_vm.heading))]\n    ),\n    _vm._v(\" \"),\n    _c(\"div\", { class: \"w-full text-1xl text-\" + _vm.primaryColor }, [\n      _vm._v(\n        \"\\n      \" +\n          _vm._s(\"Breadcrumb nav 1\") +\n          \"\\n      >\\n      \" +\n          _vm._s(\"Breadcrumb nav 2\") +\n          \"\\n  \"\n      )\n    ])\n  ])\n}\nvar staticRenderFns = []\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack:///./src/js/components/shared/PeoplePlaceHeader.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
 /***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/shared/PersonInfo.vue?vue&type=template&id=00d3cc31&":
 /*!**************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./src/js/components/shared/PersonInfo.vue?vue&type=template&id=00d3cc31& ***!
@@ -45,6 +69,42 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return staticRenderFns; });\nvar render = function() {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _c(\n    \"div\",\n    { staticClass: \"p-8\" },\n    [\n      _c(\"people-place-header\", {\n        attrs: { heading: \"DIRECTORY\", classes: \"flex lg:hidden\" }\n      }),\n      _vm._v(\" \"),\n      _c(\"div\", { staticClass: \"flex flex-wrap justify-center\" }, [\n        _c(\n          \"div\",\n          {\n            staticClass:\n              \"flex flex-wrap justify-center \\n    lg:w-1/2 lg:order-2 md:w-full md:mb-6 md:order-1\"\n          },\n          [_c(\"mod-map\", { attrs: { place: _vm.person } })],\n          1\n        ),\n        _vm._v(\" \"),\n        _c(\n          \"div\",\n          { staticClass: \"lg:w-1/2 lg:order-1 md:w-full md:order-2\" },\n          [_c(\"person-info\", { attrs: { person: _vm.person } })],\n          1\n        )\n      ])\n    ],\n    1\n  )\n}\nvar staticRenderFns = []\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack:///./src/js/pages/people/_Person.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options");
+
+/***/ }),
+
+/***/ "./src/js/components/shared/PeoplePlaceHeader.vue":
+/*!********************************************************!*\
+  !*** ./src/js/components/shared/PeoplePlaceHeader.vue ***!
+  \********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _PeoplePlaceHeader_vue_vue_type_template_id_17fe7b72___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72& */ \"./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72&\");\n/* harmony import */ var _PeoplePlaceHeader_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./PeoplePlaceHeader.vue?vue&type=script&lang=js& */ \"./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ \"./node_modules/vue-loader/lib/runtime/componentNormalizer.js\");\n\n\n\n\n\n/* normalize component */\n\nvar component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\n  _PeoplePlaceHeader_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  _PeoplePlaceHeader_vue_vue_type_template_id_17fe7b72___WEBPACK_IMPORTED_MODULE_0__[\"render\"],\n  _PeoplePlaceHeader_vue_vue_type_template_id_17fe7b72___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"],\n  false,\n  null,\n  null,\n  null\n  \n)\n\n/* hot reload */\nif (false) { var api; }\ncomponent.options.__file = \"src/js/components/shared/PeoplePlaceHeader.vue\"\n/* harmony default export */ __webpack_exports__[\"default\"] = (component.exports);\n\n//# sourceURL=webpack:///./src/js/components/shared/PeoplePlaceHeader.vue?");
+
+/***/ }),
+
+/***/ "./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=script&lang=js&":
+/*!*********************************************************************************!*\
+  !*** ./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=script&lang=js& ***!
+  \*********************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_PeoplePlaceHeader_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/babel-loader/lib!../../../../node_modules/vue-loader/lib??vue-loader-options!./PeoplePlaceHeader.vue?vue&type=script&lang=js& */ \"./node_modules/babel-loader/lib/index.js!./node_modules/vue-loader/lib/index.js?!./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=script&lang=js&\");\n/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__[\"default\"] = (_node_modules_babel_loader_lib_index_js_node_modules_vue_loader_lib_index_js_vue_loader_options_PeoplePlaceHeader_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[\"default\"]); \n\n//# sourceURL=webpack:///./src/js/components/shared/PeoplePlaceHeader.vue?");
+
+/***/ }),
+
+/***/ "./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72&":
+/*!***************************************************************************************!*\
+  !*** ./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72& ***!
+  \***************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_PeoplePlaceHeader_vue_vue_type_template_id_17fe7b72___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../../node_modules/vue-loader/lib??vue-loader-options!./PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72& */ \"./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/shared/PeoplePlaceHeader.vue?vue&type=template&id=17fe7b72&\");\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_PeoplePlaceHeader_vue_vue_type_template_id_17fe7b72___WEBPACK_IMPORTED_MODULE_0__[\"render\"]; });\n\n/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_PeoplePlaceHeader_vue_vue_type_template_id_17fe7b72___WEBPACK_IMPORTED_MODULE_0__[\"staticRenderFns\"]; });\n\n\n\n//# sourceURL=webpack:///./src/js/components/shared/PeoplePlaceHeader.vue?");
 
 /***/ }),
 
