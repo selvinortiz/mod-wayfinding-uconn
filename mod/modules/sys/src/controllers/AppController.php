0f5d6d3bee3e4cb2b50d389ad809731511674c74
Selvin Ortiz <selvin@selvin.co>
2019-09-11 10:27:22 -0500

    Add a way for the server to send global settings to the client for initial routing of Places based on context (Mobile vs Kiosk)

--- mod/modules/sys/src/controllers/AppController.php
+++ mod/modules/sys/src/controllers/AppController.php
@@ -2,7 +2,8 @@
 
 namespace modules\sys\controllers;
 
-use Craft;
+use craft\elements\Asset;
+use craft\elements\GlobalSet;
 use craft\helpers\Json;
 use craft\web\Controller;
 use modules\sys\elements\Building;
@@ -26,9 +27,51 @@ class AppController extends Controller
             $kiosk = $kiosk ?: [];
         }
 
-        $kiosk = Json::encode($kiosk);
-        $theme = file_get_contents(dirname(__DIR__, 5).'/theme.json');
+        $kiosk    = Json::encode($kiosk);
+        $theme    = file_get_contents(dirname(__DIR__, 5).'/theme.json');
+        $settings = $this->getGlobalSettings();
 
-        return $this->renderTemplate('index', compact('kiosk', 'theme'));
+        return $this->renderTemplate('index', compact('kiosk', 'theme', 'settings'));
+    }
+
+    private function getGlobalSettings()
+    {
+        $settings  = [];
+        $globalSet = GlobalSet::find()
+            ->with(['logo', 'images'])
+            ->handle('settings')
+            ->one();
+
+        if ($globalSet)
+        {
+            $settings = $globalSet->getFieldValues();
+
+            foreach ($settings as $handle => &$value)
+            {
+                // Replace value with previously eager loaded one
+                if ($globalSet->hasEagerLoadedElements($handle))
+                {
+                    $value = $globalSet->getEagerLoadedElements($handle);
+                }
+
+                // Clean up what is returned for asset fields
+                if (is_array($value))
+                {
+                    foreach ($value as &$item)
+                    {
+                        if ($item instanceof Asset)
+                        {
+                            $item = [
+                                'url'    => $item->getUrl(),
+                                'width'  => $item->getWidth(),
+                                'height' => $item->getHeight(),
+                            ];
+                        }
+                    }
+                }
+            }
+        }
+
+        return $settings;
     }
 }
