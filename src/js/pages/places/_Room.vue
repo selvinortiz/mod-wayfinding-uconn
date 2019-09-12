45997e60efdcc51581599245587f325f2e86db34
Selvin Ortiz <selvin@selvin.co>
2019-09-12 11:40:15 -0500

    Big Campus/Building/Room routing refactoring and update of all dropdowns using custom component

--- src/js/pages/places/_Room.vue
+++ src/js/pages/places/_Room.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="p-8">
-
-    <h1>Room</h1>
-
     <div class="flex flex-wrap justify-center">
-
-      <div class="flex flex-wrap justify-center
-      lg:w-1/2 lg:order-2 md:w-full md:mb-6 md:order-1">
-
+      <div class="flex flex-wrap justify-center lg:w-1/2 lg:order-2 md:w-full md:mb-6 md:order-1">
         <mod-map :place="place"></mod-map>
-
       </div>
-
       <div class="lg:w-1/2 lg:order-1 md:w-full md:order-2">
+        <div :style="containerStyles">
+          <div class="w-full flex flex-wrap p-4">
+            <div class="w-full mb-4 text-2xl font-black">{{place.title}}</div>
 
-        <place-info :place="place"></place-info>
+            <div>
+              <img class="w-4/5 mb-4" src="http://placehold.it/500" />
 
+              <div class="font-bold mb-4">Phone Number</div>
+              <div class="font-bold">Address Line 1</div>
+              <div class="font-bold mb-4">Address Line 2</div>
+              <div>Place Description</div>
+            </div>
+          </div>
+        </div>
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
 import axios from "../../utils/Axios";
-import ModMap from '../../components/shared/ModMap.vue'
-import PlaceInfo from '../../components/shared/PlaceInfo.vue'
+import ModMap from "../../components/shared/ModMap.vue";
+import PlaceInfo from "../../components/shared/PlaceInfo.vue";
 
 export default {
   metaInfo: {
@@ -38,24 +39,25 @@ export default {
   },
   data() {
     return {
-      place: {},
+      place: {}
 
       // Not Used
       //primaryColor: this.$store.state.app.theme.main.wayfinding.text
-    }
+    };
   },
   created() {
-    axios.post("/actions/sys/wayfinding/place", {
-      id: this.$route.params.id
-    })
-    .then(response => {
-      this.place = {
-        ...response.data.place,
-        loaded: true
-      };
-      console.log(this.place);
-    })
-    .catch(error => console.error(error));
+    axios
+      .post("/actions/sys/wayfinding/place", {
+        id: this.$route.params.id
+      })
+      .then(response => {
+        this.place = {
+          ...response.data.place,
+          loaded: true
+        };
+        console.log(this.place);
+      })
+      .catch(error => console.error(error));
   }
 };
 </script>
