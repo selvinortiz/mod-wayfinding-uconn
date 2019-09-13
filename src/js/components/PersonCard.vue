c7648faa2fb829cdb3934f89a7ff328830f81712
Selvin Ortiz <selvin@selvin.co>
2019-09-12 23:52:22 -0500

    Abstract person page styles into PersonCard

---
+++ src/js/components/PersonCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <router-link class="flex" :to="{ name: 'person', params: {id: person.id} }">
+    <img class="w-full" src="/static/img/avatar.svg" />
+
+    <div class="w-full p-2">
+      <p class="font-bold text-2xl">
+        {{ person.personFirstName }}
+        {{ person.personLastName }}
+      </p>
+      <p>
+        Role
+        <!--{{ person.personFirstName }}-->
+      </p>
+      <p>
+        Suite #
+        <!--{{ person.personFirstName }}-->
+      </p>
+      <p>
+        Can't tell what goes here
+        <!--{{ person.personFirstName }}-->
+      </p>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    person: {
+      type: Object,
+      default: () => {id: null}
+    }
+  }
+}
+</script>
