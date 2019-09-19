7c204752bf543d700bbf9ca00eb5d70d3c00c083
Selvin Ortiz <selvin@selvin.co>
2019-09-19 18:07:55 -0500

    Update app vertical layout to fit everything in low res screens

--- src/js/components/partials/AppFooter.vue
+++ src/js/components/partials/AppFooter.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="flex flex-1 items-center justify-between text-center text-white">
-    <div class="py-4 px-8 flex items-end">
+    <div class="py-2 px-8 flex items-end">
       <p class="flex flex-no-wrap items-end text-base leading-none">
-        <img class="px-2" style="max-height: 48px;" src="/static/icons/qr.png" alt="">
-        <img class="px-2" style="max-height: 48px;" src="/static/icons/tap.svg" alt="">
+        <img class="px-2" style="max-height: 32px;" src="/static/icons/qr.png" alt="">
+        <img class="px-2" style="max-height: 32px;" src="/static/icons/tap.svg" alt="">
         <span class="text-left">
           <span class="block uppercase">Info to go!</span>
           <span class="block text-xs">Scan the QR Code to download the UCONN&nbsp;app</span>
         </span>
       </p>
     </div>
-    <div class="py-4 px-8">
+    <div class="py-2 px-8">
       <a @click="toggleAdaMode" class="flex items-end text-base leading-none">
         <span class="px-4 text-right">Tap for improved ADA accessability</span>
-        <img style="max-height: 48px;" src="/static/icons/ada.svg" />
+        <img style="max-height: 32px;" src="/static/icons/ada.svg" />
       </a>
     </div>
   </section>
