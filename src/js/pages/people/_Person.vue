05fe93ee951db99d203916d34c97aa0ea9673131
Selvin Ortiz <selvin@selvin.co>
2019-09-24 23:25:06 -0500

    Remove PersonInfo component and update Person view to use live data from server API🔥

--- src/js/pages/people/_Person.vue
+++ src/js/pages/people/_Person.vue
@@ -3,10 +3,41 @@
     <page-header>Directory</page-header>
     <div class="flex flex-wrap justify-center">
       <div class="flex flex-wrap justify-center lg:w-1/2 lg:order-2 md:w-full md:mb-6 md:order-1">
-       <!-- <mod-map :place="person"></mod-map> -->
+        <mod-map :place="place"></mod-map>
       </div>
       <div class="lg:w-1/2 lg:order-1 md:w-full md:order-2">
-        <person-info :person="person"></person-info>
+        <div class="w-full flex flex-wrap justify-center">
+          <div class="w-3/5 pt-4">
+            <div
+              class="text-2xl uppercase"
+              :style="`color: ${theme.colors.primary}`"
+            >{{ person.personFirstName }} {{ person.personLastName }}</div>
+
+            <div class="pt-2" v-for="role in person.personRoles" :key="role.id">
+              <p class="font-bold">{{ role.roleTitle }}</p>
+              <p>{{ role.roleDepartment[0].title }}</p>
+            </div>
+
+            <div class="pt-4">
+              <p class="font-bold">{{ person.personPhone }}</p>
+              <p class="font-bold mb-4">{{ person.personEmail }}</p>
+            </div>
+
+            <!-- <div class="font-bold">Building Name</div>
+            <div>Suite #</div>
+            <div class="mb-4">Floor #</div>-->
+
+            <div class="pt-4">
+              <p>{{ person.personCity }}, {{ person.personState }} {{ person.personZipcode }}</p>
+            </div>
+
+            <div>{{ person.personDescription }}</div>
+          </div>
+
+          <div class="w-2/5" :style="`color: `">
+            <img class="w-3/5 mb-6" style="max-width: 300px" src="/static/img/avatar.svg" />
+          </div>
+        </div>
       </div>
     </div>
   </content-loader>
@@ -14,18 +45,12 @@
 
 <script>
 import { person } from "../../utils/Axios";
-import ModMap from "../../components/ModMap.vue";
-import PersonInfo from "../../components/shared/PersonInfo.vue";
 
 export default {
   metaInfo() {
     return {
       title: this.title
     };
-  },
-  components: {
-    ModMap,
-    PersonInfo
   },
   data() {
     return {
@@ -35,28 +60,36 @@ export default {
       }
     };
   },
-  computed: {
-    title() {
-      return `${this.person.personFirstName} ${this.person.personLastName}`;
-    }
-  },
   created() {
     this.fetch();
+  },
+  computed: {
+    theme() {
+      return this.$store.state.app.theme;
+    },
+    title() {
+      return `${this.person.personFirstName} ${this.person.personLastName}`;
+    },
+    place() {
+      return this.person.loaded
+        ? this.person.personRelatedPlace[0]
+        : { id: null };
+    }
   },
   methods: {
     fetch() {
       person({ id: this.$route.params.id })
         .then(response => {
           this.person = {
-          ...response.data.person,
-          loaded: true
+            ...response.data.person,
+            loaded: true
           };
         })
         .catch(error => console.error(error));
     }
   },
   watch: {
-    '$route' (to) {
+    $route(to) {
       if (to.name === "person") {
         this.place = {};
         this.fetch();
