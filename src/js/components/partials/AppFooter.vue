5088f95170cbfe604ebb1298666fb40d38dccc34
Selvin Ortiz <selvin@selvin.co>
2019-10-07 00:12:53 -0500

    Fix QR code section in footer

--- src/js/components/partials/AppFooter.vue
+++ src/js/components/partials/AppFooter.vue
@@ -2,15 +2,14 @@
   <section class="@end flex flex-1 items-center justify-between text-center text-white">
     <div class="py-2 px-16 flex items-end">
       <p class="flex flex-no-wrap items-end text-base leading-none">
-        <img class="px-2" style="max-height: 32px;" src="/static/icons/qr.png" alt="">
-        <img class="px-2" style="max-height: 32px;" src="/static/icons/tap.svg" alt="">
+        <img class="px-2" style="max-height: 64px;" src="/static/icons/qr.png" alt="">
         <span class="text-left">
-          <span class="block uppercase">Info to go!</span>
-          <span class="block text-xs">Scan the QR Code to download the UCONN&nbsp;app</span>
+          <span class="block text-lg uppercase">Info to go!</span>
+          <span class="block text-sm">Scan the QR Code to download the UCONN&nbsp;App</span>
         </span>
       </p>
     </div>
-    <div class="py-2 px-8">
+    <div class="hidden py-2 px-8">
       <a v-if="showAdaToggle" @click="toggleAdaMode" class="flex items-end text-base leading-none">
         <span class="px-4 text-right">Tap for improved ADA accessability</span>
         <img style="max-height: 32px;" src="/static/icons/ada.svg" />
