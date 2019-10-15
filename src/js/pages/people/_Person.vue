4c0fba3a9a0ac36d6808725ad1845b48cb670c0e
ImmerGen Paul <plyrek@immergenstudios.com>
2019-10-15 16:15:47 -0500

    _person.vue changed the ratio on landscape to be 2/3 map 1/3 text and image. Gap between them was to large

--- src/js/pages/people/_Person.vue
+++ src/js/pages/people/_Person.vue
@@ -1,10 +1,10 @@
 <template>
   <content-loader :loaded="person.loaded" class="p-16">
     <section class="xl:flex flex-wrap">
-      <div class="xl:w-1/2 xl:order-1">
+      <div class="xl:w-2/3 xl:order-1">
         <mod-map :maps="place.maps" primary-map="building" class="xl:px-4"></mod-map>
       </div>
-      <div class="w-full flex flex-wrap xl:w-1/2">
+      <div class="w-full flex flex-wrap xl:w-1/3">
         <h2
           class="w-full font-thin text-4xl leading-none uppercase xl:pb-6 xl:pt-0 lg:py-8 md:py-8"
           :style="`color: ${theme.colors.primary}`"
