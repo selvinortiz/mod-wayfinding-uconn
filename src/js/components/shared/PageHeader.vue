902eab38e95112604d390117cfca50495b1a9651
ImmerGen Paul <plyrek@immergenstudios.com>
2019-10-18 15:23:18 -0500

    Change to page header to allow top headlines to full width

--- src/js/components/shared/PageHeader.vue
+++ src/js/components/shared/PageHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="font-bold text-2xl leading-tight max-w-lg" :style="`color: ${$store.state.app.theme.colors.primary}`">
+    <h1 class="font-bold text-2xl leading-tight w-full" :style="`color: ${$store.state.app.theme.colors.primary}`">
       <slot></slot>
     </h1>
   </div>
